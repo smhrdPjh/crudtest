d03869d26ef9bfcf3d243ba3c52d348eaebb41a4
smhrdPjh <pjh01082037858@gmail.com>
2023-01-03 23:39:23 +0900

    리액트 완료

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import AppRouter from './component/route/RouterComponent';
+import NavBar from './component/route/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
      <AppRouter/>
     </div>
   );
